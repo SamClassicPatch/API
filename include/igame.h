3b73fbb0c34bce672a723a57dd384ac6185f43d9
Dreamy Cecil <21009796+DreamyCecil@users.noreply.github.com>
2024-06-07 23:12:57 +0300

    Sort and rename virtual methods in IClassicsGame interface.

--- include/igame.h
+++ include/igame.h
@@ -23,12 +23,6 @@ public:
   // Get enum index of a gamemode from the array of possible gamemodes
   virtual int GetGameMode(int i) = 0;
 
-  // Get amount of available local players
-  virtual int GetLocalPlayerCount(void) = 0;
-
-  // Get amount of available player profiles
-  virtual int GetProfileCount(void) = 0;
-
   // Console state
   virtual int GetConState(void) = 0;
   virtual void SetConState(int iState) = 0;
@@ -36,22 +30,6 @@ public:
   // Computer state
   virtual int GetCompState(void) = 0;
   virtual void SetCompState(int iState) = 0;
-
-  // Index of a player profile used for starting a singleplayer game
-  virtual int GetPlayerForSP(void) = 0;
-  virtual void SetPlayerForSP(int iPlayer) = 0;
-
-  // Split-screen configuration mode in the game menu
-  virtual int GetMenuSplitCfg(void) = 0;
-  virtual void SetMenuSplitCfg(int iConfiguration) = 0;
-
-  // Split-screen configuration mode that the game starts with
-  virtual int GetStartSplitCfg(void) = 0;
-  virtual void SetStartSplitCfg(int iConfiguration) = 0;
-
-  // Split-screen configuration mode in the currently active game
-  virtual int GetCurrentSplitCfg(void) = 0;
-  virtual void SetCurrentSplitCfg(int iConfiguration) = 0;
 
   // Active game state
   virtual bool IsGameOn(void) = 0;
@@ -65,25 +43,64 @@ public:
   virtual bool GetFirstLoading(void) = 0;
   virtual void SetFirstLoading(bool bState) = 0;
 
-  // Index of a player profile for modifications via in-game menu
-  virtual int GetMenuPlayer(int i) = 0;
-  virtual void SetMenuPlayer(int i, int iPlayer) = 0;
+  //================================================================================================//
+  // Split-screen configuration modes
+  //
+  // "iConfiguration" should be set using values from CGame::SplitScreenCfg enum
+  //================================================================================================//
 
-  // Indices of player profiles that will be starting the game
-  virtual int GetStartPlayer(int i) = 0;
-  virtual void SetStartPlayer(int i, int iPlayer) = 0;
+  // Mode in the game menu for setting up the game
+  virtual int GetMenuSplitCfg(void) = 0;
+  virtual void SetMenuSplitCfg(int iConfiguration) = 0;
 
-  // Reset all start players
-  virtual void ResetStartPlayers(void) = 0;
+  // Mode that the game starts in
+  virtual int GetStartSplitCfg(void) = 0;
+  virtual void SetStartSplitCfg(int iConfiguration) = 0;
 
-  // Copy menu player indices into start player indices
-  virtual void SetStartPlayersFromMenuPlayers(void) = 0;
+  // Mode in the currently active game
+  virtual int GetCurrentSplitCfg(void) = 0;
+  virtual void SetCurrentSplitCfg(int iConfiguration) = 0;
 
-  // Check if local player is active
+  //================================================================================================//
+  // Local players & player profiles
+  //
+  // Local players are used for setting up players in split-screen mode using unique player profiles.
+  // Indices of local players in the fixed-size array are referred to as "iPlayer".
+  //
+  // Player profiles are unique customizable characters used for distinguishing local players and
+  // different clients on a server. Each profile uses its own save files and controls.
+  // Indices of player profiles in the fixed-size array are referred to as "iProfile".
+  //================================================================================================//
+
+  // Get maximum amount of local players that can be used in split-screen
+  virtual int GetLocalPlayerCount(void) = 0;
+
+  // Get maximum amount of player profiles that the game client supports
+  virtual int GetProfileCount(void) = 0;
+
+  // Check if a local player is active during current game
   virtual bool IsLocalPlayerActive(int iPlayer) = 0;
 
-  // Get index of a local player
-  virtual int GetLocalPlayerIndex(int iPlayer) = 0;
+  // Get player profile index associated with a local player
+  virtual int GetProfileFromLocalPlayer(int iPlayer) = 0;
+
+  // Index of a player profile used for starting a singleplayer game
+  virtual int GetProfileForSP(void) = 0;
+  virtual void SetProfileForSP(int iProfile) = 0;
+
+  // Indices of player profiles for modifications via in-game menu
+  virtual int GetProfileForMenu(int iPlayer) = 0;
+  virtual void SetProfileForMenu(int iPlayer, int iProfile) = 0;
+
+  // Indices of player profiles that will be starting the game
+  virtual int GetProfileForStart(int iPlayer) = 0;
+  virtual void SetProfileForStart(int iPlayer, int iProfile) = 0;
+
+  // Reset profiles for all local players
+  virtual void ResetStartProfiles(void) = 0;
+
+  // Copy indices of menu profiles into start profiles
+  virtual void SetStartProfilesFromMenuProfiles(void) = 0;
 };
 
 // Get pointer to the game interface
