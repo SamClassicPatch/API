4facb0555fc4a1e9ded972240abf7d58ef5c6eae
Dreamy Cecil <21009796+DreamyCecil@users.noreply.github.com>
2025-03-14 23:50:07 +0300

    Add extension properties for establishing communication with plugins.

--- include/iextensions.h
+++ include/iextensions.h
@@ -50,6 +50,110 @@ PATCH_API HPatchPlugin PATCH_CALLTYPE ClassicsExtensions_GetExtensionByIndex(int
 // Retrieve a specific extension by its identifier
 PATCH_API HPatchPlugin PATCH_CALLTYPE ClassicsExtensions_GetExtensionByName(const char *strExtension);
 
+// One extension property in the array
+struct ExtensionProp_t
+{
+  const char *m_strProperty;
+
+  enum EType {
+    k_EType_Bool   = 0,
+    k_EType_Int    = 1,
+    k_EType_Float  = 2,
+    k_EType_Double = 3,
+    k_EType_String = 4,
+    k_EType_Data   = 5,
+  } m_eType;
+
+  union {
+    bool        _bool;
+    int         _int;
+    float       _float;
+    double      _double;
+    const char *_string;
+    void       *_data;
+  } m_value;
+
+  // Constructors for each type because initializer lists {} don't work with unions
+  ExtensionProp_t(const char *str, bool        bValue) : m_strProperty(str), m_eType(k_EType_Bool)   { m_value._bool   = bValue; };
+  ExtensionProp_t(const char *str, int         iValue) : m_strProperty(str), m_eType(k_EType_Int)    { m_value._int    = iValue; };
+  ExtensionProp_t(const char *str, float       fValue) : m_strProperty(str), m_eType(k_EType_Float)  { m_value._float  = fValue; };
+  ExtensionProp_t(const char *str, double      fValue) : m_strProperty(str), m_eType(k_EType_Double) { m_value._double = fValue; };
+  ExtensionProp_t(const char *str, const char *sValue) : m_strProperty(str), m_eType(k_EType_String) { m_value._string = sValue; };
+  ExtensionProp_t(const char *str, void       *pValue) : m_strProperty(str), m_eType(k_EType_Data)   { m_value._data   = pValue; };
+};
+
+// Extension properties symbols
+#define EXTENSIONMODULE_PROPSARRAY  ClassicsPatchExtension_PropsArray
+#define EXTENSIONMODULE_PROPSCOUNT  ClassicsPatchExtension_PropsCount
+
+// Define array of extension properties
+// Example usage:
+//    CLASSICSPATCH_EXTENSION_PROPS_BEGIN {
+//      ExtensionProp_t( "my_bool_property", true ),
+//      ExtensionProp_t( "my_int_property", -1 ),
+//      ExtensionProp_t( "my_float_property", 0.0f ),
+//      ExtensionProp_t( "my_string_property", "Hello, world!" ),
+//    } CLASSICSPATCH_EXTENSION_PROPS_END;
+#define CLASSICSPATCH_EXTENSION_PROPS_BEGIN MODULE_API ExtensionProp_t EXTENSIONMODULE_PROPSARRAY[] =
+#define CLASSICSPATCH_EXTENSION_PROPS_END ; MODULE_API const size_t EXTENSIONMODULE_PROPSCOUNT = ARRAYCOUNT(EXTENSIONMODULE_PROPSARRAY)
+
+// Try to find some extension property; returns NULL if not found
+PATCH_API ExtensionProp_t *PATCH_CALLTYPE ClassicsExtensions_FindProperty(HPatchPlugin hPlugin, const char *strProperty);
+
+// Try to find some extension property of a specific type; returns NULL if not found
+//
+// It may be slightly faster than ClassicsExtensions_FindProperty() with subsequent type checking
+// because it immediately skips properties with mismatching types without comparing name strings.
+PATCH_API ExtensionProp_t *PATCH_CALLTYPE ClassicsExtensions_FindPropertyOfType(HPatchPlugin hPlugin, const char *strProperty, ExtensionProp_t::EType eType);
+
+// Retrieve a boolean value from some extension property
+// Returns false if the property for this value type cannot be found
+PATCH_API bool PATCH_CALLTYPE ClassicsExtensions_GetBool(HPatchPlugin hPlugin, const char *strProperty, bool *pValue);
+
+// Retrieve an integer value from some extension property
+// Returns false if the property for this value type cannot be found
+PATCH_API bool PATCH_CALLTYPE ClassicsExtensions_GetInt(HPatchPlugin hPlugin, const char *strProperty, int *pValue);
+
+// Retrieve a float value from some extension property
+// Returns false if the property for this value type cannot be found
+PATCH_API bool PATCH_CALLTYPE ClassicsExtensions_GetFloat(HPatchPlugin hPlugin, const char *strProperty, float *pValue);
+
+// Retrieve a float value from some extension property
+// Returns false if the property for this value type cannot be found
+PATCH_API bool PATCH_CALLTYPE ClassicsExtensions_GetDouble(HPatchPlugin hPlugin, const char *strProperty, double *pValue);
+
+// Retrieve a string value from some extension property
+// Returns false if the property for this value type cannot be found
+PATCH_API bool PATCH_CALLTYPE ClassicsExtensions_GetString(HPatchPlugin hPlugin, const char *strProperty, const char **pValue);
+
+// Retrieve a pointer to some data from some extension property
+// Returns false if the property for this value type cannot be found
+PATCH_API bool PATCH_CALLTYPE ClassicsExtensions_GetData(HPatchPlugin hPlugin, const char *strProperty, void **pValue);
+
+// Set a boolean value to some extension property
+// Returns false if the property for this value type cannot be found
+PATCH_API bool PATCH_CALLTYPE ClassicsExtensions_SetBool(HPatchPlugin hPlugin, const char *strProperty, bool bValue);
+
+// Set an integer value to some extension property
+// Returns false if the property for this value type cannot be found
+PATCH_API bool PATCH_CALLTYPE ClassicsExtensions_SetInt(HPatchPlugin hPlugin, const char *strProperty, int iValue);
+
+// Set a float value to some extension property
+// Returns false if the property for this value type cannot be found
+PATCH_API bool PATCH_CALLTYPE ClassicsExtensions_SetFloat(HPatchPlugin hPlugin, const char *strProperty, float fValue);
+
+// Set a float value to some extension property
+// Returns false if the property for this value type cannot be found
+PATCH_API bool PATCH_CALLTYPE ClassicsExtensions_SetDouble(HPatchPlugin hPlugin, const char *strProperty, double fValue);
+
+// Set a string value to some extension property
+// Returns false if the property for this value type cannot be found
+PATCH_API bool PATCH_CALLTYPE ClassicsExtensions_SetString(HPatchPlugin hPlugin, const char *strProperty, const char *strValue);
+
+// Set a pointer to some data to some extension property
+// Returns false if the property for this value type cannot be found
+PATCH_API bool PATCH_CALLTYPE ClassicsExtensions_SetData(HPatchPlugin hPlugin, const char *strProperty, void *pValue);
+
 //================================================================================================//
 // Virtual Classics Patch API
 //================================================================================================//
@@ -60,6 +164,23 @@ public:
   virtual int GetExtensionCount(void) { return ClassicsExtensions_GetExtensionCount(); };
   virtual HPatchPlugin GetExtensionByIndex(int iExtension) { return ClassicsExtensions_GetExtensionByIndex(iExtension); };
   virtual HPatchPlugin GetExtensionByName(const char *strExtension) { return ClassicsExtensions_GetExtensionByName(strExtension); };
+
+  virtual ExtensionProp_t *FindProperty(HPatchPlugin hPlugin, const char *strProperty) { return ClassicsExtensions_FindProperty(hPlugin, strProperty); };
+  virtual ExtensionProp_t *FindPropertyOfType(HPatchPlugin hPlugin, const char *strProperty, ExtensionProp_t::EType eType) { return ClassicsExtensions_FindPropertyOfType(hPlugin, strProperty, eType); };
+
+  virtual bool GetBool(HPatchPlugin hPlugin, const char *strProperty, bool *pValue) { return ClassicsExtensions_GetBool(hPlugin, strProperty, pValue); };
+  virtual bool GetInt(HPatchPlugin hPlugin, const char *strProperty, int *pValue) { return ClassicsExtensions_GetInt(hPlugin, strProperty, pValue); };
+  virtual bool GetFloat(HPatchPlugin hPlugin, const char *strProperty, float *pValue) { return ClassicsExtensions_GetFloat(hPlugin, strProperty, pValue); };
+  virtual bool GetDouble(HPatchPlugin hPlugin, const char *strProperty, double *pValue) { return ClassicsExtensions_GetDouble(hPlugin, strProperty, pValue); };
+  virtual bool GetString(HPatchPlugin hPlugin, const char *strProperty, const char **pValue) { return ClassicsExtensions_GetString(hPlugin, strProperty, pValue); };
+  virtual bool GetData(HPatchPlugin hPlugin, const char *strProperty, void **pValue) { return ClassicsExtensions_GetData(hPlugin, strProperty, pValue); };
+
+  virtual bool SetBool(HPatchPlugin hPlugin, const char *strProperty, bool bValue) { return ClassicsExtensions_SetBool(hPlugin, strProperty, bValue); };
+  virtual bool SetInt(HPatchPlugin hPlugin, const char *strProperty, int iValue) { return ClassicsExtensions_SetInt(hPlugin, strProperty, iValue); };
+  virtual bool SetFloat(HPatchPlugin hPlugin, const char *strProperty, float fValue) { return ClassicsExtensions_SetFloat(hPlugin, strProperty, fValue); };
+  virtual bool SetDouble(HPatchPlugin hPlugin, const char *strProperty, double fValue) { return ClassicsExtensions_SetDouble(hPlugin, strProperty, fValue); };
+  virtual bool SetString(HPatchPlugin hPlugin, const char *strProperty, const char *strValue) { return ClassicsExtensions_SetString(hPlugin, strProperty, strValue); };
+  virtual bool SetData(HPatchPlugin hPlugin, const char *strProperty, void *pValue) { return ClassicsExtensions_SetData(hPlugin, strProperty, pValue); };
 };
 
 #endif // CLASSICSPATCH_IEXTENSIONS_H
